7b7cdab6538ba0597fa9aaae2f0a0b548a305891
John Conley <john.conley@modopayments.com>
2018-01-09 11:52:04 -0600

    build: removed git install

--- build_scripts/retrieve_git_meta.sh
+++ build_scripts/retrieve_git_meta.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Retrieving git metadata from: $CODEBUILD_SOURCE_REPO_URL:$CODEBUILD_SOURCE_VERSION"
+echo "Retrieving git metadata from: $CODEBUILD_SOURCE_REPO_URL:$CODEBUILD_RESOLVED_SOURCE_VERSION"
+git status
 git clone -n $CODEBUILD_SOURCE_REPO_URL --separate-git-dir ./.git tempclone
-git checkout $CODEBUILD_SOURCE_VERSION --detach --force
+git checkout $CODEBUILD_RESOLVED_SOURCE_VERSION --detach --force
 rm -rf tempclone
