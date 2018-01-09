e8f7113eb3b76594692d56a15a972437a99d83a7
John Conley <john.conley@modopayments.com>
2018-01-09 12:04:16 -0600

    build: only run publish on pipeline

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -2,4 +2,8 @@
 source "$NVM_DIR/nvm.sh"
 npm install
 npm test
-npm run release
+if [$CODEBUILD_INITIATOR==$DEPLOYMENT_PIPELINE]; then
+    npm run release
+else
+    echo "Skipping publish on non-deployment build"
+fi
