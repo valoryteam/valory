e3da79ec35940289f821cad517e55abce3594f4a
John Conley <john.conley@modopayments.com>
2018-01-09 12:08:09 -0600

    build: added missing quotes

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -2,7 +2,7 @@
 source "$NVM_DIR/nvm.sh"
 npm install
 npm test
-if [$CODEBUILD_INITIATOR==$DEPLOYMENT_PIPELINE]; then
+if ["$CODEBUILD_INITIATOR"=="$DEPLOYMENT_PIPELINE"]; then
     npm run release
 else
     echo "Skipping publish on non-deployment build"
