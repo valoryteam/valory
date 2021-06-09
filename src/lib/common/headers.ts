f00696fd9e8c94680842f3fcab82e44c70fe7c9d
John Conley <john.conley@nordicsemi.no>
2021-06-09 11:39:12 -0700

    fix: remove broken type

--- src/lib/common/headers.ts
+++ src/lib/common/headers.ts
@@ -79,7 +79,7 @@ export const ROUTES_VERSION = 2;
 export const GLOBAL_ENTRY_KEY = "VALORY_DATA";
 
 export interface ApiResponse extends ApiExchange {
-    statusCode: HttpStatusCodeLiteral;
+    statusCode: number;
 }
 
 export interface ApiRequest extends ApiExchange {
@@ -113,63 +113,3 @@ export interface ApiAdaptor {
 
     shutdown(): void;
 }
-
-export type HttpStatusCodeLiteral =
-    | 100
-    | 101
-    | 102
-    | 200
-    | 201
-    | 202
-    | 203
-    | 204
-    | 205
-    | 206
-    | 207
-    | 208
-    | 226
-    | 300
-    | 301
-    | 302
-    | 303
-    | 304
-    | 305
-    | 307
-    | 308
-    | 400
-    | 401
-    | 402
-    | 403
-    | 404
-    | 405
-    | 406
-    | 407
-    | 408
-    | 409
-    | 410
-    | 411
-    | 412
-    | 413
-    | 414
-    | 415
-    | 416
-    | 417
-    | 418
-    | 422
-    | 423
-    | 424
-    | 426
-    | 428
-    | 429
-    | 431
-    | 500
-    | 501
-    | 502
-    | 503
-    | 504
-    | 505
-    | 506
-    | 507
-    | 508
-    | 510
-    | 511;
